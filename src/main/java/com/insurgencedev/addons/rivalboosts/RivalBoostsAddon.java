2bc2c2750ae2d285e791fcfa4e24ab0f16b8b6f4
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-02-12 23:05:59 -0500

    fixed addon not loading

--- src/main/java/com/insurgencedev/addons/rivalboosts/RivalBoostsAddon.java
+++ src/main/java/com/insurgencedev/addons/rivalboosts/RivalBoostsAddon.java
@@ -4,9 +4,11 @@ import com.insurgencedev.addons.rivalboosts.listeners.RivalHoeEventListener;
 import com.insurgencedev.addons.rivalboosts.listeners.RivalPickaxeEventListener;
 import com.insurgencedev.addons.rivalboosts.listeners.RivalRodEventListener;
 import com.insurgencedev.addons.rivalboosts.listeners.RivalSwordEventListener;
+import org.insurgencedev.insurgencesets.api.addon.ISetsAddon;
 import org.insurgencedev.insurgencesets.api.addon.InsurgenceSetsAddon;
 import org.insurgencedev.insurgencesets.libs.fo.Common;
 
+@ISetsAddon(name = "RivalBoostAddon", version = "1.0.1", author = "InsurgenceDev", description = "All-in-one tool boost")
 public class RivalBoostsAddon extends InsurgenceSetsAddon {
 
     public static final String NAMESPACE = "Rival";
